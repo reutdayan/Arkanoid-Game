f5c7b8d935138554073f98780d5602e244452c76
reutdayan <83058858+reutdayan@users.noreply.github.com>
2022-09-20 10:49:00 +0300

    Update Ass6Game.java

--- src/Ass6Game.java
+++ src/Ass6Game.java
@@ -17,20 +17,10 @@ public class Ass6Game {
     public static void main(String[] args) {
         GameFlow game = new GameFlow();
         List<LevelInformation> levels = new ArrayList<>();
-        for (String s : args) {
-            if (s.equals("1")) {
-                levels.add(new Level1());
-            }
-            if (s.equals("2")) {
-                levels.add(new Level2());
-            }
-            if (s.equals("3")) {
-                levels.add(new Level3());
-            }
-            if (s.equals("4")) {
-                levels.add(new Level4());
-            }
-        }
+        levels.add(new Level1());
+        levels.add(new Level2());
+        levels.add(new Level3());
+        levels.add(new Level4());
         game.runLevels(levels);
     }
 }
